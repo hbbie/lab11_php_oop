24217a7cceba8ca77293d7ed72ea9d212f76a93e
hbbie <nflxxy9@gmail.com>
2025-12-17 13:10:53 +0700

    Create sidebar.php

---
+++ template/sidebar.php
@@ -0,0 +1,7 @@
+<div style="background:#eee;padding:10px;border-radius:6px;">
+    <h4>Menu</h4>
+    <ul>
+        <li><a href="/lab11_php_oop/artikel/index">Daftar Artikel</a></li>
+        <li><a href="/lab11_php_oop/artikel/tambah">Tambah Artikel</a></li>
+    </ul>
+</div>
